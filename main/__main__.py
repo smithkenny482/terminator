f84718a9f6258bd12ad0c4de4c789cdcf3b1080e
smithkenny482 <119724953+smithkenny482@users.noreply.github.com>
2023-05-01 15:53:36 +0800

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,6 +17,18 @@ import telegram
  
 from datetime import datetime, timedelta
 from telegram.ext import JobQueue
+
+bot_token = '5602217970:AAFN0Sj8bakFutyYZ2__cnZX5lIy-lOiDpA'
+ 
+updater = Updater(token=bot_token, use_context=True)
+dispatcher = updater.dispatcher
+ 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+ 
+logger = logging.getLogger()
+ 
+updater.start_polling(drop_pending_updates = True)
  
 group_user_ids = {}
 job_queue = updater.job_queue
